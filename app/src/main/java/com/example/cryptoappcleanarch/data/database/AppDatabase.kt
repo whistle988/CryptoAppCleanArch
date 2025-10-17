cfd7fa46afa8a39e790a6bda54e804d2518e840f
vasileva_ea3 <vasileva_ea3@albank.ru>
2025-10-17 17:59:33 +0900

    FixPresentationLayer

--- app/src/main/java/com/example/cryptoappcleanarch/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/cryptoappcleanarch/data/database/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [CoinInfoDbModel::class], version = 1, exportSchema = false)
+@Database(entities = [CoinInfoDbModel::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     companion object {
 
@@ -21,7 +21,8 @@ abstract class AppDatabase : RoomDatabase() {
                         context,
                         AppDatabase::class.java,
                         DB_NAME
-                    ).build()
+                    )   .fallbackToDestructiveMigration() // при обновлении версии, старая БД будет удалена
+                        .build()
                 db = instance
                 return instance
             }
